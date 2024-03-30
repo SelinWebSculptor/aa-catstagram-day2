95b26996e126faadfec6a312b8cbd5b0b01a8550
Royce Calvin <royce20002006@yahoo.com>
2024-03-30 14:50:38 -0500

    part 2 started

--- public/index.js
+++ public/index.js
@@ -1,1 +1,29 @@
-// Your code here 
+window.onload = (e) => {
+    let catPic;
+    let header = document.createElement('h1');
+    header.textContent = 'Kitten Pic';
+    header.setAttribute('id', 'pageTitle')
+    document.body.appendChild(header);
+    let button = document.createElement('button');
+    button.textContent = 'new kitten';
+    button.setAttribute('action','get');
+    document.body.appendChild(button);
+    button.addEventListener('click', event => {
+      document.getElementsByTagName('img').setAttribute = ``;
+  }, 1000);
+
+    })
+    
+    fetch('https://api.thecatapi.com/v1/images/search').then(res => {
+      return res.json().then(data => {
+        const KittenImg = data[0].url;
+        console.log(KittenImg);
+        let randomCat = document.createElement('img');
+        randomCat.setAttribute('src', KittenImg);
+        randomCat.setAttribute('id', 'randomCatImg')
+        document.body.appendChild(randomCat);
+      });
+
+      
+    })
+}
