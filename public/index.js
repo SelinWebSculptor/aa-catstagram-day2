bc52c478daed95d00d232a7421acfc1641ef1c4b
Royce Calvin <royce20002006@yahoo.com>
2024-03-30 15:25:48 -0500

    button fixed

--- public/index.js
+++ public/index.js
@@ -1,29 +1,39 @@
+let KittenImg;
+let randomCat;
+let changeImg = () => {
+  let img = document.getElementById('randomCatImg');
+  fetching();
+  randomCat.setAttribute('src', KittenImg);
+}
+
+const fetching = () => {
+  fetch('https://api.thecatapi.com/v1/images/search?').then(res => {
+    return res.json().then(data => {
+      KittenImg = data[0].url;
+      randomCat.setAttribute('id', 'randomCatImg')
+      randomCat.setAttribute('src', KittenImg)
+      console.log(randomCat)
+      console.log(KittenImg);
+    });
+  })
+}
 window.onload = (e) => {
-    let catPic;
-    let header = document.createElement('h1');
-    header.textContent = 'Kitten Pic';
-    header.setAttribute('id', 'pageTitle')
-    document.body.appendChild(header);
-    let button = document.createElement('button');
-    button.textContent = 'new kitten';
-    button.setAttribute('action','get');
-    document.body.appendChild(button);
-    button.addEventListener('click', event => {
-      document.getElementsByTagName('img').setAttribute = ``;
-  }, 1000);
-
-    })
+  let header = document.createElement('h1');
+  header.textContent = 'Kitten Pic';
+  header.setAttribute('id', 'pageTitle')
+  document.body.appendChild(header);
+  let button = document.createElement('button');
+  button.textContent = 'new kitten';
+  button.setAttribute('action', 'get');
+  document.body.appendChild(button);
+  button.addEventListener('click', event => {
+    changeImg();
     
-    fetch('https://api.thecatapi.com/v1/images/search').then(res => {
-      return res.json().then(data => {
-        const KittenImg = data[0].url;
-        console.log(KittenImg);
-        let randomCat = document.createElement('img');
-        randomCat.setAttribute('src', KittenImg);
-        randomCat.setAttribute('id', 'randomCatImg')
-        document.body.appendChild(randomCat);
-      });
+  })
+  
+  randomCat = document.createElement('img');
+  document.body.appendChild(randomCat);
+  fetching();
 
-      
-    })
+  
 }
